2ba8a400991167ba126555508d273fb7f26e9d7d
Laboratorio1 <maricielo.luna@ucsp.edu.pe>
2023-09-21 17:44:43 -0500

    agregue un nuevo ejercicio

---
+++ LAB1/ejercicio2-22.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "*****\n****\n***\n **\n*\n " <<endl; // Imprime una piramide inversa de asteriscos "*", cada linea representa una fila de asteriscos con un número decreciente de tal modo formando la piramide inversa.
+    return 0;
+}
